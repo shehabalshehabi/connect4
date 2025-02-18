9afa785defab274f1d1b7755fb0959039831db40
shehabalshehabi <33294930+shehabalshehabi@users.noreply.github.com>
2025-02-18 19:20:21 +0300

    add win check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,28 +7,91 @@ export default function Home() {
   return (Game());
 }
 
+enum gameResultEnums{
+  InProgress,
+  Draw,
+  Player1Win,
+  Player2Win,
+}
+
 function Game() {
-  const [player1Turn, setPlayer1Turn] = useState<boolean>(true);
-  const [boardState, setBoardState] = useState<number[][]>(Array(7).fill(0).map(()=>Array(6).fill(0)));
+  const [gameHistory, setGameHistory] = useState<number[][][]>([Array(7).fill(0).map(()=>Array(6).fill(0))]);
+  const [currentPly, setCurrentPly] = useState<number>(0);
+  const [gameResult, setGameResult] = useState<gameResultEnums>(gameResultEnums.InProgress);
+  function checkWin(boardState: number[][], col:number, row:number){
+    const player = boardState[col][row];
+    // Vertical
+    for (let i=row-3; i<=row; i++){
+      if (i < 0){
+        break;
+      }
+      if (i == row){
+        return true
+      }
+      if (boardState[col][row] != player){
+        break;
+      }
+    }
+
+    var run = 0;
+    var x, y = 0;
+    for (let vertdelta=-1; vertdelta<=1; vertdelta++){
+      run = 0;
+      for (let delta=-3; delta<=3; delta++){
+        x = col + delta;
+        y = row + vertdelta * delta;
+        if (x < 0 || x >= 7 || y < 0 || y >= 6){
+          run = 0;
+          continue
+        }
+        if (boardState[x][y] == player){
+          run += 1;
+          if (run >= 4){
+            return true;
+          }
+        } else {
+          run = 0;
+        }
+      }
+    }
+    return false;
+  }
   function playTurn(colNum:number){
+    if (gameResult != gameResultEnums.InProgress){
+      return;
+    }
+    const boardState = gameHistory[currentPly];
+    const player1Turn = !(currentPly%2);
     for (let i = 0; i<boardState[colNum].length; i++){
       if (boardState[colNum][i] == 0) {
-        setBoardState((prevBoardState)=>{
-          const newBoardState = [...prevBoardState];
+        setGameHistory((prevGameHistory)=>{ 
+          const newGameHistory = prevGameHistory.slice(0,currentPly+1);
+          const newBoardState = [...prevGameHistory[currentPly]];
           newBoardState[colNum][i] = player1Turn ? 1 : 2;
-          return newBoardState;
+          newGameHistory.push(newBoardState);
+          if (checkWin(newBoardState, colNum, i)){
+            if (player1Turn){
+              setGameResult(gameResultEnums.Player1Win);
+            } else {
+              setGameResult(gameResultEnums.Player2Win);
+            }
+          } else {
+            if (currentPly == 6 * 7 - 1){
+              setGameResult(gameResultEnums.Draw)
+            }
+          }
+          return newGameHistory;
         })
-        setPlayer1Turn((player1Turn)=>!player1Turn)
+        setCurrentPly((currentPly)=>currentPly+1);
         break;
       }
     }
   }
-  return <div className="game-container"><Board boardState={boardState} playTurn={playTurn}></Board></div>
+  return <div className="game-container"><Board boardState={gameHistory.at(-1)!} playTurn={playTurn}></Board></div>
 }
 
 function Board({boardState, playTurn}:{boardState:number[][], playTurn:(colNum:number)=>void}) {
   const cols = boardState.map((columnState, colNum)=><Column columnState={columnState} key={colNum} playColumn={()=>playTurn(colNum)}></Column>)
-  console.log(cols.toString());
   return <div className="board">{cols}</div>
 }
 
